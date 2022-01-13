a8a7b0a0352f973a18d5cbca9933b2ffad7e0077
hogg21 <89107241+hogg21@users.noreply.github.com>
2022-01-14 00:18:44 +0200

    Add files via upload

--- lesson3/task3/src/index.js
+++ lesson3/task3/src/index.js
@@ -1,17 +1,17 @@
 import store from "./store.js";
-import { addUser, removeUser } from "./user.actions.js";
+import { setUser, removeUser } from "./user.actions.js";
 import { addProduct, removeProduct } from "./cart.actions.js";
 import { setLanguage } from "./language.actions.js";
 
 
 store.subscribe(() => console.log(store.getState()));
 
-store.dispatch(addUser({ name: 'Bob' }));
+store.dispatch(setUser({ name: 'Bob' }));
 store.dispatch(removeUser())
-store.dispatch(addUser({ name: 'Hogg' }));
+store.dispatch(setUser({ name: 'Hogg' }));
 store.dispatch(removeUser())
 store.dispatch(addProduct({ id: 76, name: 'milk' }));
 store.dispatch(addProduct({ id: 77, name: 'chocolate' }));
 store.dispatch(removeProduct(77))
-store.dispatch(addUser({ name: 'Bob' }));
+store.dispatch(setUser({ name: 'Bob' }));
 store.dispatch(setLanguage('jp'));
