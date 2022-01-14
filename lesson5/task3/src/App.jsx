a0d69f61a79ad6abf11ecb6b7c8c57fb8aec7e8b
hogg21 <89107241+hogg21@users.noreply.github.com>
2022-01-15 00:51:20 +0200

    Add files via upload

--- lesson5/task3/src/App.jsx
+++ lesson5/task3/src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import UsersList from './users/UsersList.jsx';
 import { Provider } from "react-redux";
-import store from "./store.js";
-import users from './users/users.js'
-
+import UsersList from "./users/UsersList";
+import store from "./store";
 const App = () => {
-    return (
-        <Provider store={store}>
-            <UsersList users={users}></UsersList>
-        </Provider>
-    )
-}
+  return (
+    <Provider store={store}>
+      <UsersList />
+    </Provider>
+  );
+};
+
 export default App;
