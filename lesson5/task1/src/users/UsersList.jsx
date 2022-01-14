4c39c89ea2975991446536fd69138c3929d03c26
hogg21 <89107241+hogg21@users.noreply.github.com>
2022-01-15 00:39:57 +0200

    Add files via upload

--- lesson5/task1/src/users/UsersList.jsx
+++ lesson5/task1/src/users/UsersList.jsx
@@ -6,7 +6,7 @@ import { userListSelector, currentPageSelector } from '../users.selectors.js';
 
 const ITEMS_PER_PAGE = 3;
 
-const UsersList = ({ users, goPrevPage, goNextPage, currentPage }) => {
+const UsersList = ({ users, goPrev, goNext, currentPage }) => {
   const startIndex = currentPage * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
   const usersToRender = users.slice(startIndex, endIndex);
@@ -14,8 +14,8 @@ const UsersList = ({ users, goPrevPage, goNextPage, currentPage }) => {
   return (
     <div>
       <Pagination
-        goNext={goNextPage}
-        goPrev={goPrevPage}
+        goNext={goNext}
+        goPrev={goPrev}
         currentPage={currentPage}
         totalItems={users.length}
         itemsPerPage={ITEMS_PER_PAGE}
